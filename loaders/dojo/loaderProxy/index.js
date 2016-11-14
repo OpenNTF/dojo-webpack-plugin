0bc13d3501d88ce03e205bb2355df1d7125c5a35
chuckd <chuckd@us.ibm.com>
2016-11-14 15:50:59 -0500

    Fix loaderProxy issue

--- loaders/dojo/loaderProxy/index.js
+++ loaders/dojo/loaderProxy/index.js
@@ -10,6 +10,13 @@ module.exports = function(content) {
 	var segments = this._module.absMid.split("!");
 	var name = segments[segments.length-1];
 	var deps = query.deps ? query.deps.split(",") : [];
+	var issuer = this._module.issuer;
+	if (issuer) {
+		issuerAbsMid = this._compilation.findModule(issuer).absMid;
+	}
+	if (!issuerAbsMid) {
+		issuerAbsMid = this._module.absMid || "";
+	}
 	var buf = [];
 	var runner = require.resolve("./runner.js").replace(/\\/g, "/");
 	buf.push("define([\"" + loader + "\", \"" + runner + "\"");
@@ -20,7 +27,7 @@ module.exports = function(content) {
 	buf.push("runner(");
 	buf.push("\tloader,");
 	buf.push("\t\"" + name + "\",");
-	buf.push("\t__webpack_require__.dj.c(" + JSON.stringfy(this._module.issuer.id) + ")");
+	buf.push("\t__webpack_require__.dj.c(\"" + issuerAbsMid + "\")");
 	buf.push(");");
 	buf.push("});");
 	return buf.join("\n");
