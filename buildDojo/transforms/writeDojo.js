b54de9cf8039e4e8eadb6d1cce20d39b8affc2b1
chuckd <chuckd@us.ibm.com>
2016-11-17 16:20:31 -0500

    Copyright headers

--- buildDojo/transforms/writeDojo.js
+++ buildDojo/transforms/writeDojo.js
@@ -1,3 +1,24 @@
+/*
+ * (C) Copyright IBM Corp. 2012, 2016 All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * This transform is an adaptation of the write transform in dojo/util that has been modified to build 
+ * the dojo loader only, without copying any other sources to the output directory and to avoid minifying
+ * the output file.
+ */
 define([
 	"util/build/buildControl",
 	"util/build/fileUtils",
