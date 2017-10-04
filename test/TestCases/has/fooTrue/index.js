daa993b8bc40053f97a1b255b6e03662f2f057e6
chuckd <chuckd@us.ibm.com>
2017-10-04 16:52:09 -0400

    Unit test cases for dojo/has loader extension

---
+++ test/TestCases/has/fooTrue/index.js
@@ -0,0 +1,13 @@
+define(["dojo/has", "dojo/has!foo?./a:./b"], function(has, m1) {
+  it("should load true module", function(done) {
+    has("webpack").should.be.true;
+    m1.should.be.eql("a");
+    has.add("foo", false, true);
+    require(["dojo/has!foo?./c:./d"], function(m2) {
+      // module should have been set at build time and not changed just because foo changed
+      has("foo").should.be.false;
+      m2.should.be.eql("c");
+      done();
+    });
+  });
+});
