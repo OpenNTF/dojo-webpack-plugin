b779bc79ebf98821ca5018af039a67360176ee1b
chuckd <chuckd@us.ibm.com>
2017-10-04 21:08:15 -0400

    More test coverage

--- test/TestCases/dependencies/constDeps/index.js
+++ test/TestCases/dependencies/constDeps/index.js
@@ -28,7 +28,9 @@ define("exports,module,./dep".split(","), function(exports, module, dep) {
 			exceptionThrown = true;
 		}
 		exceptionThrown.should.be.true;
-		require('require,./asyncDep'.split(','), function(require, asyncDep) {
+		require('require,module,exports,./asyncDep'.split(','), function(require, reqModule, reqExports, asyncDep) {
+			reqModule.id.should.be.eql(module.id);
+			reqExports.should.be.eql(exports);
 			asyncDep.should.be.eql("asyncDep");
 			// context require
 			require("./asyncDep").should.be.eql(asyncDep);
