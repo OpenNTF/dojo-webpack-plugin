e38ec61d674be23ecb7e707bd621e2d4d4a38af4
chuckd <chuckd@us.ibm.com>
2017-10-12 12:58:01 -0400

    Unit tests for dojo/has plugin name resolution

--- test/TestCases/has/fooFalse/index.js
+++ test/TestCases/has/fooFalse/index.js
@@ -1,9 +1,10 @@
-define(["dojo/has", "dojo/has!foo?./a:./b"], function(has, m1) {
+define(["dojo/has", "dojo/has!foo?./a:./b", "dojo/has!foo?undef"], function(has, m1, undef) {
 
   it("should load false module", function(done) {
     has("webpack").should.be.true;
     has("foo").should.be.false;
     m1.should.be.eql("b");
+		(typeof undef === 'undefined').should.be.true;
     has.add("foo", true, true);
     require(["dojo/has!foo?./c:./d"], function(m2) {
       // module should have been set at build time and not changed just because foo changed
--- test/TestCases/has/fooTrue/index.js
+++ test/TestCases/has/fooTrue/index.js
@@ -1,7 +1,8 @@
-define(["dojo/has", "dojo/has!foo?./a:./b"], function(has, m1) {
+define(["dojo/has", "dojo/has!foo?./a:./b", "dojo/has!foo?:undef"], function(has, m1, undef) {
   it("should load true module", function(done) {
     has("webpack").should.be.true;
     m1.should.be.eql("a");
+		(typeof undef === 'undefined').should.be.true;
     has.add("foo", false, true);
     require(["dojo/has!foo?./c:./d"], function(m2) {
       // module should have been set at build time and not changed just because foo changed
