7bbaa4f22ebe242c41b691a3403ca966918b5fd6
chuckd <chuckd@us.ibm.com>
2017-12-27 18:36:22 -0500

    constNamedModules unit test failes when run using webpack < 3.7

--- test/TestCases/dependencies/constNamedModules/index.js
+++ test/TestCases/dependencies/constNamedModules/index.js
@@ -1,3 +1,8 @@
+const version = require("webpack/package.json").version;
+const versionParts = version.split(".").map(part => {
+	return parseInt(part);
+});
+
 define("named1", [], function() {
 	return "named1";
 });
@@ -14,19 +19,25 @@ define("named4", [], function() {
 	return "named4";
 });
 
-define("named1,named2".split(","), function(named1, named2) {
+// Need fix in webpack version 3.7 so skip this test if older version
+// https://github.com/webpack/webpack/pull/5771
+if (versionParts[0] > 3 || versionParts[0] === 3 && versionParts[1] >= 7) {
+	define("named1,named2".split(","), function(named1, named2) {
+		it("should load the named modules in const defined dependencies", function() {
+			"named1".should.be.eql(named1);
+			"named2".should.be.eql(named2);
+		});
 
-	it("should load the named modules in const defined dependencies", function() {
-		"named1".should.be.eql(named1);
-		"named2".should.be.eql(named2);
-	});
-
-	it("should load the named modules in const require dependencies", function(done) {
-		require("named3,named4".split(','), function (named3, named4) {
-			"named3".should.be.eql(named3);
-			"named4".should.be.eql(named4);
-			done();
+		it("should load the named modules in const require dependencies", function(done) {
+			require("named3,named4".split(','), function (named3, named4) {
+				"named3".should.be.eql(named3);
+				"named4".should.be.eql(named4);
+				done();
+			});
 		});
 	});
-
-});
+} else {
+	it("Skipping due to unsupported webpack version", function(done) {
+		done();
+	});
+}
