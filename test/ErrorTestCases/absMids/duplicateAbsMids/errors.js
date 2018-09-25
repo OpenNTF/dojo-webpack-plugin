74df4356df4f9e9b7af266828aef144960cdabde
Chuck Dumont <chuckd@pnp-hcl.com>
2018-09-25 12:11:55 -0400

    Fix broken test cases when using webpack >= 4.19

--- test/ErrorTestCases/absMids/duplicateAbsMids/errors.js
+++ test/ErrorTestCases/absMids/duplicateAbsMids/errors.js
@@ -1,3 +1,8 @@
+const version = require("webpack/package.json").version.split(".");
+const regexp = /Duplicate absMid/;
 module.exports = [
-	[/Duplicate absMid/]
+	[regexp]
 ];
+if (parseInt(version[0]) >= 4 && parseInt(version[1]) >= 19) {
+	module.exports.push([regexp]);
+}
