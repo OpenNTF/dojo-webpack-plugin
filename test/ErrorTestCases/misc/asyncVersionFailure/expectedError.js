ce20cae444887264aa723ad10e294b74796d789f
Chuck Dumont <chuckd@pnp-hcl.com>
2019-02-25 12:11:56 -0500

    Fix broken unit test

--- test/ErrorTestCases/misc/asyncVersionFailure/expectedError.js
+++ test/ErrorTestCases/misc/asyncVersionFailure/expectedError.js
@@ -2,7 +2,7 @@ const major = parseInt(require("webpack/package.json").version.split('.')[0]);
 const result = {};
 if (major < 4) {
 	result.test = function(error) {
-		return /Async mode require webpack 4 or greater/.test(error.message);
+		return /Async mode requires webpack 4.28.4 or greater/.test(error.message);
 	};
 }
 module.exports = result;
