3ad6e6d9dcc50abccdc83a495272f0f91d00b480
Chuck Dumont <chuckd@pnp-hcl.com>
2019-05-24 10:20:44 -0400

    Fix issue with pre-mature promise resolution of second level dependencies

--- test/TestCases/loaders/asyncProxyLoader/index.js
+++ test/TestCases/loaders/asyncProxyLoader/index.js
@@ -1,8 +1,10 @@
-define(["require", "test/asyncPlugin!foo/content.txt", "test/asyncPlugin!./content.txt", "test/asyncPlugin!"], function(req, content1, content2, content3) {
+define(["require", "test/asyncPlugin!foo/content.txt", "test/asyncPlugin!./content.txt", "test/asyncPlugin!", "test/thenableResult", "dep"], function(req, content1, content2, content3, thenableResult, dep) {
 	it("should load the resource name as the content", function(done) {
 		"Name = foo/content.txt".should.be.eql(content1);
 		"Name = ./content.txt".should.be.eql(content2);
 		"Name = ".should.be.eql(content3);
+		(typeof thenableResult.then).should.be.eql('function');
+		(typeof dep.then).should.be.eql('function');
 		done();
 	});
 	it("should load using context runtime require", function(done) {
