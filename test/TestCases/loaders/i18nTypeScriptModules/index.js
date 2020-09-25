f8e0f96a3780161bf6fec1631a0c9873b4e50ce6
Heiko Studt <heiko.studt@goldpool.org>
2020-09-25 15:31:57 +0200

    Since the bugfix with correct define usage in i18nEval, the dependency on the special language is not necessary anymore.

--- test/TestCases/loaders/i18nTypeScriptModules/index.js
+++ test/TestCases/loaders/i18nTypeScriptModules/index.js
@@ -1,4 +1,4 @@
-define(["dojo/i18n!./nls/strings", "./nls/de-at/strings"], function(strings) {
+define(["dojo/i18n!./nls/strings"], function(strings) {
 	it("should load the strings if generated with typescript (require, exports)", function() {
 		strings.hello.should.be.eql("xyz-de");
 	});
